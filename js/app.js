50e94066c6cfeedde606798ae129812f39a3f1f6
ganeshnitalikar <ganeshnitalikar@gmail.com>
2023-04-27 23:18:05 +0530

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const colors = ["Green","Red","Blue","Yellow","Orange","White","Purple",];
+const btn = document.getElementById('btn');
+const color = document.querySelector(".color");
+
+btn.addEventListener('click',function(){
+    const randomNumber =getRandomNumber();
+    console.log(randomNumber)
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber]
+})
+
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+  }
+  
